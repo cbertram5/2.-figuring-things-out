1d147caa79b1f492e7932c7bf808c45d8655b7a0
Chris Bertram <bertramc@bc.edu>
2020-09-02 19:02:36 -0400

    Initial Commit

---
+++ 2.5 correcting errors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.5 correcting errors
+//
+//  Created by Chris Bertram on 9/2/20.
+//  Copyright © 2020 Chris Bertram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
