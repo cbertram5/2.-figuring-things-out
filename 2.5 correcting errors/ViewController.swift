84f90a386a234cfcede3523c4a27bb51f082caa6
Chris Bertram <bertramc@bc.edu>
2020-09-03 17:43:49 -0400

    just finished 2.7 figuring things out

--- 2.5 correcting errors/ViewController.swift
+++ 2.5 correcting errors/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = ""
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+        imageView.image = UIImage(named: "image1")
+    }
+    
 }
 
